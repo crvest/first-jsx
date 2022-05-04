3f6663c841d596f0ee492d6ee608ec066045ec87
crvest <chad.vest@gmail.com>
2022-05-04 15:25:23 -0400

    first_jsx complete

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <li>
+        <ul>Learn React</ul>
+        <ul>Climb Mt Everest</ul>
+        <ul>Run a marathon</ul>
+        <ul>Feed the dogs</ul>
+      </li>
+    </>
   );
 }
 
